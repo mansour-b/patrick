098062e598b4c1d5d94d4f12096aafca66aafe21
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2024-10-17 17:02:47 +0200

    remove mean + load results in notebook

--- call_dicodile.py
+++ call_dicodile.py
@@ -17,7 +17,9 @@ def load_data():
     image_array = plt.imread(file_path_list[0])
     learnable_image = image_array[:, :, :-1].transpose([2, 0, 1])
 
-    return learnable_image
+    channel_per_channel_mean = np.reshape(np.mean(learnable_image, (1, 2)), (-1, 1, 1))
+
+    return learnable_image - channel_per_channel_mean
 
 
 def save_results(D_hat, z_hat, time_str):
@@ -26,50 +28,52 @@ def save_results(D_hat, z_hat, time_str):
 
     output_dir_path.mkdir(parents=True, exist_ok=True)
 
-    D_path = output_dir_path / f"D_hat_{time_str}.txt"
-    z_path = output_dir_path / f"z_hat_{time_str}.txt"
+    D_path = output_dir_path / f"D_hat_{time_str}"
+    z_path = output_dir_path / f"z_hat_{time_str}"
 
-    np.savetxt(D_path, D_hat)
-    np.savetxt(z_path, z_hat)
-
-
-learnable_image = load_data()
-
-n_atoms = 3
-atom_support = (50, 50)
-D_init = init_dictionary(
-    learnable_image, n_atoms=n_atoms, atom_support=atom_support, random_state=60
-)
-tw = tukey_window(atom_support)[None, None]  # make sure that the border values are 0
-D_init *= tw
+    np.save(D_path, D_hat)
+    np.save(z_path, z_hat)
 
 
-reg = 0.2  # regularization parameter
-n_iter = 100  # maximum number of iterations
-window = True  # when True, makes sure that the borders of the atoms are 0
-z_positive = True  # when True, requires all activations Z to be positive
-n_workers = 10  # number of workers to be used for computations
-w_world = "auto"  # number of jobs per row
-tol = 1e-3  # tolerance for minimal update size
+if __name__ == "__main__":
 
-time_str = time.strftime("%y%m%d_%H%M%S")
+    time_str = time.strftime("%y%m%d_%H%M%S")
 
+    n_atoms = 10
+    atom_support = (50, 50)
 
-D_hat, z_hat, pobj, times = dicodile(
-    learnable_image,
-    D_init,
-    reg=reg,
-    n_iter=n_iter,
-    window=window,
-    z_positive=z_positive,
-    n_workers=n_workers,
-    dicod_kwargs={"max_iter": 10000},
-    w_world=w_world,
-    tol=tol,
-    verbose=1,
-)
+    reg = 0.2  # regularization parameter
+    n_iter = 100  # maximum number of iterations
+    window = True  # when True, makes sure that the borders of the atoms are 0
+    z_positive = True  # when True, requires all activations Z to be positive
+    n_workers = 10  # number of workers to be used for computations
+    w_world = "auto"  # number of jobs per row
+    tol = 1e-3  # tolerance for minimal update size
 
-print("[DICOD] final cost : {}".format(pobj))
+    learnable_image = load_data()
 
+    D_init = init_dictionary(
+        learnable_image, n_atoms=n_atoms, atom_support=atom_support, random_state=60
+    )
+    tw = tukey_window(atom_support)[
+        None, None
+    ]  # make sure that the border values are 0
+    D_init *= tw
 
-save_results(D_hat=D_hat, z_hat=z_hat, time_str=time_str)
+    D_hat, z_hat, pobj, times = dicodile(
+        learnable_image,
+        D_init,
+        reg=reg,
+        n_iter=n_iter,
+        window=window,
+        z_positive=z_positive,
+        n_workers=n_workers,
+        dicod_kwargs={"max_iter": 10000},
+        w_world=w_world,
+        tol=tol,
+        verbose=1,
+    )
+
+    print("[DICOD] final cost : {}".format(pobj))
+
+    save_results(D_hat=D_hat, z_hat=z_hat, time_str=time_str)
