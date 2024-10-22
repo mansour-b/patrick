03398d47f5e5e75ede66656de33ed4466be298cd
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2024-10-22 17:37:01 +0200

    argument parser

--- pattern_detection_tokam/call_dicodile.py
+++ pattern_detection_tokam/call_dicodile.py
@@ -1,4 +1,5 @@
 import time
+from argparse import ArgumentParser
 
 from dicodile import dicodile
 from dicodile.update_d.update_d import tukey_window
@@ -6,12 +7,57 @@ from dicodile.utils.dictionary import init_dictionary
 
 from pattern_detection_tokam.load_and_save import load_data, save_results
 
+
+def make_parser():
+    parser = ArgumentParser()
+
+    parser.add_argument("--n_atoms", help="number of atoms")
+    parser.add_argument("--atom_support", help="size of the atoms")
+    parser.add_argument("--reg", help="regularization parameter")
+    parser.add_argument("--tol", help="tolerance for minimal update size")
+    parser.add_argument(
+        "--offset_type",
+        choices=["mean", "median", "mode", "none"],
+        help="value to be subtracted to the image before the CDL begins",
+    )
+
+    parser.add_argument(
+        "--n_iter",
+        default=100,
+        help="maximum number of iterations",
+    )
+    parser.add_argument(
+        "--window",
+        default=True,
+        help="when True, makes sure that the borders of the atoms are 0",
+    )
+    parser.add_argument(
+        "--z_positive",
+        default=True,
+        help="when True, requires all activations Z to be positive",
+    )
+    parser.add_argument(
+        "--n_workers",
+        default=10,
+        help="number of workers to be used for computations",
+    )
+    parser.add_argument(
+        "--w_world",
+        default=10,
+        help="number of jobs per row",
+    )
+
+    return parser
+
+
 if __name__ == "__main__":
 
-    time_str = time.strftime("%y%m%d_%H%M%S")
+    parser = make_parser()
+    args = parser.parse_args()
 
-    n_atoms = 10
-    atom_support = (10, 10)
+    atom_support = tuple(args.atom_support)
+
+    time_str = time.strftime("%y%m%d_%H%M%S")
 
     reg = 0.2  # regularization parameter
     n_iter = 100  # maximum number of iterations
@@ -28,24 +74,25 @@ if __name__ == "__main__":
     learnable_image = load_data(experiment, frame, offset_type)
 
     D_init = init_dictionary(
-        learnable_image, n_atoms=n_atoms, atom_support=atom_support, random_state=60
+        learnable_image,
+        n_atoms=args.n_atoms,
+        atom_support=atom_support,
+        random_state=60,
     )
-    tw = tukey_window(atom_support)[
-        None, None
-    ]  # make sure that the border values are 0
-    D_init *= tw
+    tw = tukey_window(atom_support)[None, None]
+    D_init *= tw  # make sure that the border values are 0
 
     D_hat, z_hat, pobj, times = dicodile(
         learnable_image,
         D_init,
-        reg=reg,
-        n_iter=n_iter,
-        window=window,
-        z_positive=z_positive,
-        n_workers=n_workers,
+        reg=args.reg,
+        n_iter=args.n_iter,
+        window=args.window,
+        z_positive=args.z_positive,
+        n_workers=args.n_workers,
         dicod_kwargs={"max_iter": 10000},
-        w_world=w_world,
-        tol=tol,
+        w_world=args.w_world,
+        tol=args.tol,
         verbose=1,
     )
     save_results(D_hat, z_hat, experiment, frame, time_str)
