b4adb12bbf082c8089dd71489279af8ec4f74d04
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2024-10-22 17:38:23 +0200

    remove default values from the script

--- pattern_detection_tokam/call_dicodile.py
+++ pattern_detection_tokam/call_dicodile.py
@@ -58,20 +58,10 @@ if __name__ == "__main__":
     atom_support = tuple(args.atom_support)
 
     time_str = time.strftime("%y%m%d_%H%M%S")
-
-    reg = 0.2  # regularization parameter
-    n_iter = 100  # maximum number of iterations
-    window = True  # when True, makes sure that the borders of the atoms are 0
-    z_positive = True  # when True, requires all activations Z to be positive
-    n_workers = 10  # number of workers to be used for computations
-    w_world = "auto"  # number of jobs per row
-    tol = 1e-3  # tolerance for minimal update size
-
     experiment = "interchange_nodriftwave"
     frame = 1000
-    offset_type = "none"
 
-    learnable_image = load_data(experiment, frame, offset_type)
+    learnable_image = load_data(experiment, frame, args.offset_type)
 
     D_init = init_dictionary(
         learnable_image,
