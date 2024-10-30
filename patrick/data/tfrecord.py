1e32a8c8f81436019959c2b6bdb77ec63bd829cf
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2024-10-30 17:09:47 +0100

    png image

--- patrick/data/tfrecord.py
+++ patrick/data/tfrecord.py
@@ -1,35 +1,24 @@
-import io
 from pathlib import Path
 
-import PIL
 import tensorflow as tf
 
 from patrick.data.image import Image
 from patrick.efficientdet.dataset import tfrecord_util as tfru
 
 
-def image_to_example(image: Image, image_id: int, data_dir_path: Path):
+def image_to_example(image: Image, data_dir_path: Path):
+    image_path = data_dir_path / image.name
 
-    image_width = image.width
-    image_height = image.height
-    filename = image.name
-    image_id = image_id
+    image_str = tf.io.read_file(image_path)
 
-    full_path = data_dir_path / filename
-
-    with tf.io.gfile.GFile(full_path, "rb") as fid:
-        encoded_jpg = fid.read()
-
-    encoded_jpg_io = io.BytesIO(encoded_jpg)
-    image = PIL.Image.open(encoded_jpg_io)
     normalised_box_coords = compute_normalised_box_coordinates(image)
     label_list = [box._label for box in image.get_boxes()]
     feature_dict = {
-        "image/height": tfru.int64_feature(image_height),
-        "image/width": tfru.int64_feature(image_width),
-        "image/filename": tfru.bytes_feature(filename.encode("utf8")),
-        "image/encoded": tfru.bytes_feature(encoded_jpg),
-        "image/format": tfru.bytes_feature("jpeg".encode("utf8")),
+        "image/height": tfru.int64_feature(image.height),
+        "image/width": tfru.int64_feature(image.width),
+        "image/file_name": tfru.bytes_feature(image.name.encode("utf8")),
+        "image/raw": tfru.bytes_feature(image_str),
+        "image/format": tfru.bytes_feature("png".encode("utf8")),
         **{
             f"image/object/bbox/{k}": tfru.float_list_feature(v)
             for k, v in normalised_box_coords.items()
@@ -46,4 +35,9 @@ def compute_normalised_box_coordinates(image: Image) -> dict[str, list[float]]:
     xmax_list = [box.xmax / image.width for box in image.get_boxes()]
     ymin_list = [box.ymin / image.height for box in image.get_boxes()]
     ymax_list = [box.ymax / image.height for box in image.get_boxes()]
-    return {"xmin": xmin_list, "xmax": xmax_list, "ymin": ymin_list, "ymax": ymax_list}
+    return {
+        "xmin": xmin_list,
+        "xmax": xmax_list,
+        "ymin": ymin_list,
+        "ymax": ymax_list,
+    }
