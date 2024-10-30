ebd160ef25a52024ae385cc900fcc9ae16310117
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2024-10-30 11:11:43 +0100

    make movie

--- patrick/plot.py
+++ patrick/plot.py
@@ -1,7 +1,23 @@
-import matplotlib.pyplot as plt
+import time
 
+import imageio
+import matplotlib.pyplot as plt
+from moviepy.video.io.bindings import mplfig_to_npimage
+
+from patrick import PATRICK_DIR_PATH
 from patrick.data.annotation import Annotation, Polyline
 from patrick.data.image import Image
+
+
+def make_movie(image_list: list[Image], image_dir_name: str, fps: int):
+
+    time_str = time.strftime("%y%m%d_%H%M%S")
+    movie_path = PATRICK_DIR_PATH / f"misc/image_dir_name_{time_str}"
+
+    with imageio.get_writer(movie_path, fps=fps) as writer:
+        for image in image_list:
+            fig = plot_image(image, image_dir_name, return_figure=True)
+            writer.append_data(mplfig_to_npimage(fig))
 
 
 def plot_image(
@@ -9,6 +25,7 @@ def plot_image(
     image_dir_name: str = None,
     cmap: str = "gray",
     annotation_color: str = "tab:red",
+    return_figure: bool = False,
 ):
     image_array = image.get_image_array(image_dir_name)
     fig, ax = plt.subplots(1, 1)
@@ -17,6 +34,8 @@ def plot_image(
     for annotation in image._annotations:
         plot_annotation(ax, annotation, color=annotation_color)
     fig.show()
+    if return_figure:
+        return fig
 
 
 def plot_annotation(ax, annotation: Annotation, color: str):
