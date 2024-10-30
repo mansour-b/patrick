f7653d102c86144c2ab73afe2a6007dd986b9c9e
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2024-10-30 14:24:15 +0100

    merge load save modules

--- patrick/load_and_save.py
+++ patrick/load_and_save.py
@@ -56,3 +56,34 @@ def save_results(D_hat, z_hat, experiment, frame, time_str):
 
     np.save(D_path, D_hat)
     np.save(z_path, z_hat)
+
+
+def load_dict_and_activations(
+    experiment: str, frame: int, mode_or_timestamp: str = "latest", verbose: int = 0
+):
+
+    data_path = Path.home() / "data"
+    pattern_detection_path = data_path / "pattern_detection_tokam"
+    dict_path = (
+        pattern_detection_path / "learned_dictionaries" / f"{experiment}_frame_{frame}"
+    )
+
+    if mode_or_timestamp == "latest":
+        time_str_list = sorted(
+            {
+                f"{fpath.stem.split('_')[2]}_{fpath.stem.split('_')[3]}"
+                for fpath in dict_path.glob("*")
+            }
+        )
+        time_str = time_str_list[-1]
+    else:
+        time_str = mode_or_timestamp
+
+    if verbose >= 1:
+        print(f"Loaded from: {dict_path}")
+        print(f"Timestamp: {time_str}")
+
+    D_hat = np.load(dict_path / f"D_hat_{time_str}.npy")
+    z_hat = np.load(dict_path / f"z_hat_{time_str}.npy")
+
+    return D_hat, z_hat
