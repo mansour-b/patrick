82761659c98e7f215c2682a79b0297f4f92634af
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2024-10-30 16:02:13 +0100

    deserialise image list

--- patrick/data/operations.py
+++ patrick/data/operations.py
@@ -3,3 +3,7 @@ from patrick.data.image import Image
 
 def serialise_image_list(image_list: list[Image]) -> list[dict]:
     return [image.to_dict() for image in image_list]
+
+
+def deserialise_image_list(image_as_dict_list: list[dict]) -> list[Image]:
+    return [Image.from_dict(image_as_dict) for image_as_dict in image_as_dict_list]
