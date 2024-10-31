6520dd8e0a31d34789448a78dbc5dc89ebf2ac1a
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2024-10-31 09:46:39 +0100

    polyline to box

--- patrick/data/operations.py
+++ patrick/data/operations.py
@@ -1,3 +1,4 @@
+from patrick.data.annotation import Box, Polyline
 from patrick.data.image import Image
 
 
@@ -7,3 +8,14 @@ def serialise_image_list(image_list: list[Image]) -> list[dict]:
 
 def deserialise_image_list(image_as_dict_list: list[dict]) -> list[Image]:
     return [Image.from_dict(image_as_dict) for image_as_dict in image_as_dict_list]
+
+
+def polyline_to_box(polyline: Polyline) -> Box:
+    points = polyline._point_list
+    xmin = min(point[0] for point in points)
+    xmax = max(point[0] for point in points)
+    ymin = min(point[1] for point in points)
+    ymax = max(point[1] for point in points)
+    return Box(
+        label=polyline._label, x=xmin, y=ymin, width=xmax - xmin, height=ymax - ymin
+    )
