e3f5b6746bb70acae2822bbda5e8abe728af65e3
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2024-10-31 10:56:45 +0100

    fix

--- patrick/data/tfrecord.py
+++ patrick/data/tfrecord.py
@@ -9,7 +9,7 @@ def image_to_example(image: Image, data_dir_name: str):
     image_bytes = image.get_image_array(data_dir_name).tobytes()
 
     normalised_box_coords = compute_normalised_box_coordinates(image)
-    label_list = [box._label for box in image.get_boxes()]
+    label_list = [box._label.encode("utf8") for box in image.get_boxes()]
     feature_dict = {
         "image/height": tfru.int64_feature(image._height),
         "image/width": tfru.int64_feature(image._width),
@@ -28,10 +28,10 @@ def image_to_example(image: Image, data_dir_name: str):
 
 
 def compute_normalised_box_coordinates(image: Image) -> dict[str, list[float]]:
-    xmin_list = [box.xmin / image.width for box in image.get_boxes()]
-    xmax_list = [box.xmax / image.width for box in image.get_boxes()]
-    ymin_list = [box.ymin / image.height for box in image.get_boxes()]
-    ymax_list = [box.ymax / image.height for box in image.get_boxes()]
+    xmin_list = [box.xmin / image._width for box in image.get_boxes()]
+    xmax_list = [box.xmax / image._width for box in image.get_boxes()]
+    ymin_list = [box.ymin / image._height for box in image.get_boxes()]
+    ymax_list = [box.ymax / image._height for box in image.get_boxes()]
     return {
         "xmin": xmin_list,
         "xmax": xmax_list,
