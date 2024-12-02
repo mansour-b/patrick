88468372328e0c18dd914b74a132e85224a1df36
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2024-12-02 11:50:19 +0100

    metadata classes

---
+++ patrick/entities/metadata.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+
+
+class Metadata(ABC):
+    pass
+
+
+class Frame(Metadata):
+    pass
+
+
+class Movie(Metadata):
+    pass
+
+
+class Annotation(Metadata):
+    pass
+
+
+class Box(Annotation):
+    pass
+
+
+class Keypoint(Annotation):
+    pass
+
+
+class Track(Annotation):
+    pass
