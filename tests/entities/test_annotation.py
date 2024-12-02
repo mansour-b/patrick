12fb2f93bf126110a515579516dcc6db328d7559
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2024-12-02 15:56:24 +0100

    final fixes to make tests work

--- tests/entities/test_annotation.py
+++ tests/entities/test_annotation.py
@@ -2,5 +2,5 @@ from patrick.entities.annotation import Box
 
 
 def test_create_box():
-    box = Box(label="blob")
+    box = Box(label="blob", x=0, y=0, width=1, height=1, score=1)
     assert box.label == "glob"
