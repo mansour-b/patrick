022e1831a3026ab4ba893a82c2506b6938b5683c
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2024-12-05 14:09:28 +0100

    restructuration

--- patrick/use_cases/track_structures.py
+++
@@ -1,6 +0,0 @@
-from patrick import Movie, Tracker
-
-
-def track_structures(movie: Movie, tracker: Tracker) -> Movie:
-    tracks = tracker.make_tracks(movie)
-    return Movie(frame_list=movie.frame_list, track_list=tracks)
