1713cea6fafca55aadfe1e26f4da7b35d574a4d6
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2024-12-13 15:56:25 +0100

    test nnmodel

--- tests/core/entities/test_detection.py
+++ tests/core/entities/test_detection.py
@@ -1,6 +1,16 @@
+from __future__ import annotations
+
 import numpy as np
 
-from patrick.core import Array, Frame, Model, NeuralNet
+from patrick.core import (
+    Annotation,
+    Array,
+    Box,
+    Frame,
+    Model,
+    NeuralNet,
+    NNModel,
+)
 
 
 class TestModel:
@@ -38,3 +48,69 @@ class TestNeuralNet:
     def test_call(self):
         net = self.make_net()
         assert np.equal(net(np.zeros(3)), np.zeros(3)).max()
+
+
+class TestNNModel:
+    @staticmethod
+    def make_model():
+        class DumbNN(NeuralNet):
+            def __call__(self, input_array: Array) -> Array:
+                return input_array
+
+        class DumbNNModel(NNModel):
+
+            def pre_process(self, frame: Frame) -> Array:
+                return frame.image_array
+
+            def post_process(self, net_predictions: Array) -> list[Annotation]:
+                return [
+                    Box(label="blob", x=1, y=1, width=1, height=1, score=1.0)
+                ]
+
+        return DumbNNModel(
+            net=DumbNN(),
+            label_map={"blob": 1},
+            model_parameters={},
+        )
+
+    def test_init(self):
+        model = self.make_model()
+        assert model.label_map == {"blob": 1}
+        assert model.model_parameters == {}
+
+    def test_pre_process(self):
+        model = self.make_model()
+        frame = Frame(
+            name="frame_0",
+            width=16,
+            height=16,
+            annotations=[],
+            image_array=np.ones((16, 16)),
+        )
+        assert np.equal(model.pre_process(frame), np.ones((16, 16))).max()
+
+    def test_post_process(self):
+        model = self.make_model()
+        assert model.post_process(np.zeros(3)) == [
+            Box(label="blob", x=1, y=1, width=1, height=1, score=1.0)
+        ]
+
+    def test_predict(self):
+        model = self.make_model()
+        frame = Frame(
+            name="frame_0",
+            width=16,
+            height=16,
+            annotations=[],
+            image_array=np.ones((16, 16)),
+        )
+        expected_result = Frame(
+            name="frame_0",
+            width=16,
+            height=16,
+            annotations=[
+                Box(label="blob", x=1, y=1, width=1, height=1, score=1.0)
+            ],
+            image_array=np.ones((16, 16)),
+        )
+        assert model.predict(frame) == expected_result
